f196907cd9d39d3b40ac3e524c5892980c1809e6
PatriceAlan <peckpatricealan@gmail.com>
2024-04-21 00:22:26 +0200

    Explorer component modifications

--- src/Components/Explorer/Explorer.jsx
+++ src/Components/Explorer/Explorer.jsx
@@ -1,43 +1,42 @@
-import React from 'react';
+import React from "react";
+import Carousel from 'react-bootstrap/Carousel';
 import './Explorer.css';
 import image6 from '../../assets/image6.jpg';
 import image7 from '../../assets/image7.jpg';
 import image8 from '../../assets/image8.jpg';
 import Navbar from '../Navbar/Navbar';
 
-const Carousel = () => {
+const explorer = () => {
     return (
-      
-    <div className="carousel">
-        <Navbar />
-            <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
-                <ol className="carousel-indicators">
-                    <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                </ol>
-                <div className="carousel-inner">
-                    <div className="carousel-item active">
-                        <img className="d-block w-100" src={image6} alt="First slide"/>
-                    </div>
-                    <div className="carousel-item">
-                        <img className="d-block w-100" src={image7} alt="Second slide"/>
-                    </div>
-                    <div className="carousel-item">
-                        <img className="d-block w-100" src={image8} alt="Third slide"/>
-                    </div>
-                </div>
-                <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span className="sr-only">Précédent</span>
-                </a>
-                <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span className="sr-only">Suivant</span>
-                </a>
-            </div>
-    </div>
-);
+        <div className="explorer-carousel">
+            <Navbar />
+            <Carousel>
+                <Carousel.Item interval={1000}>
+                    <img src={image6} alt="Premiere Slide" />
+                    <Carousel.Caption>
+                        <h3>Titre de la premiere slide</h3>
+                        <p>Description de la premiere slide</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+
+                <Carousel.Item interval={500}>
+                    <img src={image7} alt="Deuxieme Slide" />
+                    <Carousel.Caption>
+                        <h3>Titre de la deuxieme slide</h3>
+                        <p>Description de la deuxieme slide</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+
+                <Carousel.Item interval={500}>
+                    <img src={image8} alt="Troisieme Slide" />
+                    <Carousel.Caption>
+                        <h3>Titre de la troisieme slide</h3>
+                        <p>Description de la troisieme slide</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+        </div>
+    );
 }
 
-export default Carousel;
+export default explorer;
